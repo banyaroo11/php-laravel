55866248fa31d7eacd7ae95785ff8ddcb6e71d19
Banyar Oo <mtmintern.banyaroo@gmail.com>
2026-06-08 17:00:12 +0630

    Update routes

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles', function() {
+    return 'Article List!';
+});
+
+Route::get('/articles/details/{id}', function(int $id) {
+    return "Details of article $id!";
+});
